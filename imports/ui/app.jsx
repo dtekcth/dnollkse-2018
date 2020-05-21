9e54d2674fd65d22702058827bf2c761a892ae27
Alexander Arvidsson <alexander@arvidson.nu>
2020-05-21 19:55:32 +0200

    Oops

--- imports/ui/app.jsx
+++ imports/ui/app.jsx
@@ -102,12 +102,12 @@ class App extends Component {
             content="width=device-width, initial-scale=1.0, minimum-scale=0.5"
           />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="icon" href="/favicon.ico?v=6" />
           <link rel="manifest" href="/site.webmanifest">
-          <meta name="msapplication-TileColor" content="#da532c">
+          <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#FA6607" />
 
         </Helmet>
